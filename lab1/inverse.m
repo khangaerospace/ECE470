fdbbbab52a2f9d5324b15a8d8de549361ff18ecd
Khang Nguyen <62628865+khangaerospace@users.noreply.github.com>
2025-10-12 00:15:12 -0400

    change inverse.m

--- lab1/inverse.m
+++ lab1/inverse.m
@@ -1,21 +1,34 @@
 function q = inverse(H, myrobot)
-DH = myrobot;
-Rd = H(1:3, 1:3);
-od = H(1:3, 4); 
-o = od - R*[0;0; DH.d(6)];
+    DH = myrobot;
+    Rd = H(1:3, 1:3);
+    od = H(1:3, 4); 
+    o = od - Rd*[0;0; DH.d(6)];
+    
+    
+    
+    D = (o(1)^2+o(2)^2-DH.d(2)^2+(o(3)-DH.d(1))^2-DH.a(2)^2-DH.d(4)^2)/(2*DH.a(2)*DH.d(4));
+    
+    q(1) = atan2(o(2),o(1)) - atan2(-DH.d(2), real(sqrt(o(1)^2+o(2)^2-DH.d(2)^2)));
+    q(3) = atan2(D, real(sqrt(1-D^2)));
+    q(2) = atan2(o(3)-DH.d(1), real(sqrt(o(1)^2+o(2)^2-DH.d(2)^2))) - atan2(-DH.d(4)*cos(q(3)),DH.a(2)+DH.d(4)*sin(q(3)));
+    
+    H3_0 = eye(4);
+    for i = 1:3
+        alpha = DH.alpha(i);
+        theta = q(i);
+        a = DH.a(i);
+        d = DH.d(i);
+        A = [ cos(theta), -sin(theta)*cos(alpha),  sin(theta)*sin(alpha), a*cos(theta);
+              sin(theta),  cos(theta)*cos(alpha), -cos(theta)*sin(alpha), a*sin(theta);
+              0,           sin(alpha),            cos(alpha),            d;
+              0,           0,                     0,                     1 ];
+        H3_0 = H3_0 * A;
+    end
+    R3_0 = H3_0(1:3,1:3);
 
-
-disp(R)
-D = (o(1)^2+o(2)^2-DH.d(2)^2+(o(3)-DH.d(1))^2-DH.a(2)^2-DH.d(4)^2)/(2*DH.a(2)*DH.d(4));
-
-q(1) = atan2(o(2),o(1)) - atan2(-DH.d(2), real(sqrt(o(1)^2+o(2)^2-DH.d(2)^2)));
-q(3) = atan2(D, real(sqrt(1-D^2)));
-q(2) = atan2(o(3)-DH.d(1), real(sqrt(o(1)^2+o(2)^2-DH.d(2)^2))) - atan2(-DH.d(4)*cos(q(3)),DH.a(2)+DH.d(4)*sin(q(3)));
-
-
-
-q(4) = atan2(R(2,3), R(1,3));
-q(5) = atan2(real(sqrt(1-R(3,3)^2)),R(3,3));
-q(6) = atan2(R(3,2), -R(3,1));
+    R = R3_0' * Rd;
+    q(4) = atan2(R(2,3), R(1,3));
+    q(5) = atan2(sqrt(max(1 - R(3,3)^2,0)), R(3,3));   % equivalent to acos(R(3,3))
+    q(6) = atan2(R(3,2), -R(3,1));
 
 end
